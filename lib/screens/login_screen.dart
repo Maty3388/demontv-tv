95fba65f3db3a6ba2241b8d59baf8bd31940455d
Maty3388 <Eithan100522@gmail.com>
2026-05-17 20:24:50 -0300

    fix: remove debug, improve TV navigation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -35,14 +35,7 @@ class _State extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Stack(children: [
-      _buildTV(),
-      Positioned(top: 10, right: 10, child: Container(
-        color: Colors.red,
-        padding: const EdgeInsets.all(8),
-        child: Text("W:${size.width.toInt()} H:${size.height.toInt()}", style: const TextStyle(color: Colors.white, fontSize: 14)))),
-    ]);
+    return _buildTV();
   }
 
   Widget _buildTV() => Scaffold(
