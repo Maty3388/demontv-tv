9c7a2321c48f75d99d8569d53586d9313c891c02
Maty3388 <Eithan100522@gmail.com>
2026-05-22 22:02:38 -0300

    feat: media_kit player with 4K HLS DASH support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:media_kit/media_kit.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 import 'package:flutter/services.dart';
 import 'theme/app_theme.dart';
@@ -9,6 +10,7 @@ import 'screens/main_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  MediaKit.ensureInitialized();
   WakelockPlus.enable();
   SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
