794140de09e93967f1db76b9b7f6c2d55ccdf16b
Maty3388 <Eithan100522@gmail.com>
2026-06-02 10:01:54 -0300

    fix: import settings_screen agregado

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'home_screen.dart';
+import 'settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/services.dart';
